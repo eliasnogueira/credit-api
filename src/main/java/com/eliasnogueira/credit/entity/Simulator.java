0662f3edfed7fd6d1f9e40066a991798d0691e1e
elias.nogueira@gmail.com <9365657321ea49f8473753a689d824727b8b1aec>
2020-11-23 19:56:29 +0100

    pattern constraint removed

--- src/main/java/com/eliasnogueira/credit/entity/Simulator.java
+++ src/main/java/com/eliasnogueira/credit/entity/Simulator.java
@@ -35,7 +35,6 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
