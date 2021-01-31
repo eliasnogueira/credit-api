f9b9b685830ae5bc01815e4d70182a286ae3cff0
Elias Nogueira <elias.nogueira@gmail.com>
2021-01-31 15:02:03 +0100

    removed @EnableSwagger2 annotation

--- src/main/java/com/eliasnogueira/credit/swagger/SwaggerConfig.java
+++ src/main/java/com/eliasnogueira/credit/swagger/SwaggerConfig.java
@@ -24,9 +24,9 @@
 
 package com.eliasnogueira.credit.swagger;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.concurrent.ListenableFuture;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -34,10 +34,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
