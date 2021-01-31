86ac2e948abadfcaaed0cc3936d78d5a070e6424
Elias Nogueira <elias.nogueira@gmail.com>
2021-01-31 15:01:45 +0100

    added @ApiModelProperty examples

--- src/main/java/com/eliasnogueira/credit/entity/Simulator.java
+++ src/main/java/com/eliasnogueira/credit/entity/Simulator.java
@@ -24,7 +24,13 @@
 
 package com.eliasnogueira.credit.entity;
 
-import java.math.BigDecimal;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -35,11 +41,8 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import javax.validation.constraints.Pattern;
+import java.math.BigDecimal;
 
 @Data
 @Builder(access = AccessLevel.PUBLIC)
@@ -47,36 +50,44 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(uniqueConstraints = {
-    @UniqueConstraint(name = "cpf_unique", columnNames = "cpf")
+        @UniqueConstraint(name = "cpf_unique", columnNames = "cpf")
 })
 public class Simulator {
 
+    @ApiModelProperty(hidden = true)
     @Id
     @GeneratedValue
     private Long id;
 
+    @ApiModelProperty(name = "Name", position = 1, example = "John Doe")
     @NotNull(message = "Name cannot be empty")
     @NotBlank(message = "Name cannot be empty")
     private String name;
 
+    @ApiModelProperty(name = "CPF", position = 2, example = "9709323014")
     @NotNull(message = "CPF cannot be empty")
     @NotBlank(message = "CPF cannot be empty")
     private String cpf;
 
+    @ApiModelProperty(name = "Email", position = 3, example = "john.doe@gmail.com")
     @NotNull(message = "E-mail cannot be empty")
     @Email
+    @Pattern(regexp = ".+@.+\\.[a-z]+", message = "E-mail must be valid")
     private String email;
 
+    @ApiModelProperty(name = "Amount", position = 4, example = "1200")
     @NotNull(message = "Amount cannot be empty")
     @Min(value = 1000, message = "Amount must be equal or greater than $ 1.000")
     @Max(value = 40000, message = "Amount must be equal or less than than $ 40.000")
     private BigDecimal amount;
 
+    @ApiModelProperty(name = "Installments", position = 5, example = "3")
     @NotNull(message = "Installments cannot be empty")
     @Min(value = 2, message = "Installments must be equal or greater than 2")
     @Max(value = 48, message = "Installments must be equal or less than 48")
     private Integer installments;
 
+    @ApiModelProperty(name = "Insurance", position = 5, example = "true")
     @NotNull(message = "One of the insurance options must be selected")
     private Boolean insurance;
 }
