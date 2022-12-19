8479c73fe5488c71dc5cd76522a9d294ff138ef3
Elias Nogueira <elias.nogueira@gmail.com>
2022-12-19 20:32:59 +0100

    fix springboot 3 breaking changes

--- src/main/java/com/eliasnogueira/credit/entity/Restriction.java
+++ src/main/java/com/eliasnogueira/credit/entity/Restriction.java
@@ -24,9 +24,9 @@
 
 package com.eliasnogueira.credit.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/main/java/com/eliasnogueira/credit/entity/Simulation.java
+++ src/main/java/com/eliasnogueira/credit/entity/Simulation.java
@@ -24,25 +24,24 @@
 
 package com.eliasnogueira.credit.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
-
 @Data
 @Builder(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
