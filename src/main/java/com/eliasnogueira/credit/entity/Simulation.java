16c343d3507ea80319562caa7f1fe1d87c888c15
eliasnogueira <elias.nogueira@gmail.com>
2024-03-31 21:56:05 +0200

    chore: removed unused imports

--- src/main/java/com/eliasnogueira/credit/entity/Simulation.java
+++ src/main/java/com/eliasnogueira/credit/entity/Simulation.java
@@ -34,7 +34,6 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
--- src/test/java/com/eliasnogueira/credit/tests/AssertJExamplesTest.java
+++ src/test/java/com/eliasnogueira/credit/tests/AssertJExamplesTest.java
@@ -1,25 +1,25 @@
 package com.eliasnogueira.credit.tests;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.eliasnogueira.credit.data.model.Simulation;
 import com.eliasnogueira.credit.dto.SimulationDto;
-import java.math.BigDecimal;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class AssertJExamplesTest {
     @Test
     @Disabled
     void recursiveComparisonTest() {
         var simulation1 = Simulation.builder().name("John").cpf("9582728395")
-            .email("john@gmail.com").amount(new BigDecimal("500")).installments(1)
-            .insurance(false).build();
+                .email("john@gmail.com").amount(new BigDecimal("500")).installments(1)
+                .insurance(false).build();
 
         var simulation2 = SimulationDto.builder().name("John").cpf("9582728395")
-            .email("john@gmail.com").amount(new BigDecimal("500")).installments(1)
-            .insurance(false).build();
+                .email("john@gmail.com").amount(new BigDecimal("500")).installments(1)
+                .insurance(false).build();
 
         assertThat(simulation1).usingRecursiveComparison().isEqualTo(simulation2);
     }
