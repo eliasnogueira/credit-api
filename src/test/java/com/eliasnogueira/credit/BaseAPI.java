5a3d3076eb4fc2a5e374efd98db0d981a2dce1c3
eliasnogueira <elias.nogueira@gmail.com>
2024-03-31 21:52:37 +0200

    chore: loading data from test properties to initialize restassured

--- src/test/java/com/eliasnogueira/credit/BaseAPI.java
+++ src/test/java/com/eliasnogueira/credit/BaseAPI.java
@@ -26,25 +26,41 @@ package com.eliasnogueira.credit;
 import io.restassured.RestAssured;
 import io.restassured.config.SSLConfig;
 import io.restassured.path.json.config.JsonPathConfig.NumberReturnType;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static io.restassured.RestAssured.basePath;
-import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.config;
-import static io.restassured.RestAssured.port;
 import static io.restassured.config.JsonConfig.jsonConfig;
 import static io.restassured.config.RestAssuredConfig.newConfig;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource("/application-test.properties")
 @ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 public abstract class BaseAPI {
-    @BeforeAll
-    public static void beforeAllTests() {
-        baseURI = "http://localhost";
-        basePath = "/api/v1";
-        port = 8088;
+
+    @Value("${credit-api.test.base-uri}")
+    private String baseURI;
+
+    @Value("${credit-api.test.base-path}")
+    private String basePath;
+
+    @Value("${credit-api.test.port}")
+    private int port;
+
+    /*
+     * This is done using @BeforeEach instead of @BeforeAll because Spring does not support the @Value for static fields
+     */
+    @BeforeEach
+    void beforeAllTests() {
+        RestAssured.baseURI = baseURI;
+        RestAssured.basePath = basePath;
+        RestAssured.port = port;
 
         // solve the problem with big decimal assertions
         config = newConfig().
