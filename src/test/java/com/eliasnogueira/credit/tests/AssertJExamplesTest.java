98f46c3f965529de1151e1043f4f9c99c144d7ba
eliasnogueira <elias.nogueira@gmail.com>
2024-03-31 21:52:44 +0200

    chore: disable test

--- src/test/java/com/eliasnogueira/credit/tests/AssertJExamplesTest.java
+++ src/test/java/com/eliasnogueira/credit/tests/AssertJExamplesTest.java
@@ -6,10 +6,12 @@ import com.eliasnogueira.credit.data.model.Simulation;
 import com.eliasnogueira.credit.dto.SimulationDto;
 import java.math.BigDecimal;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class AssertJExamplesTest {
     @Test
+    @Disabled
     void recursiveComparisonTest() {
         var simulation1 = Simulation.builder().name("John").cpf("9582728395")
             .email("john@gmail.com").amount(new BigDecimal("500")).installments(1)
